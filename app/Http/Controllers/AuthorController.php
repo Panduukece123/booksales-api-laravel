a1bc1c39e143dc004dd24b48f76bb20de562cb5d
Pandu Nugraha Saputra <pandunugrahasaputra2005@gmail.com>
2025-10-17 17:59:59 +0700

    Implement CRUD functionality for authors, books, and genres; add validation and JSON responses in controllers; update API routes and database seeder.

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function PHPUnit\Framework\isEmpty;
 
 class AuthorController extends Controller
 {
     public function index(){
 
         $authors = Author::all();
+
+        if($authors->isEmpty()){
+            return response()->json([
+                'succes'=> true,
+                "message"=>'Data is empty'
+
+            ], 200);
+        }
 
         return response()->json([
             'succes'=> true,
@@ -18,13 +29,53 @@ class AuthorController extends Controller
 
         ],200);
 
+
+        
         // return view('author', ['authors'=> $authors]);
         
         // data dummy
         // $data = new Author();
-
-        // $authors = $data->getAuthors();
-
         
+        // $authors = $data->getAuthors();
+        
+        
+    }
+    public function store(Request $request){
+        //Validator 
+
+        $validatorAuthor = Validator::make($request->all(),[
+            'name'=> 'required|string|max:100',
+            'photo'=>'required|image|mimes:jpeg,jpg,png|max:2048',
+            'bio'=> 'required|string|max:250'
+        ]);
+
+        //2.  Check Valid
+
+        if($validatorAuthor->fails()){
+            return response()->json([
+                'succes'=>false,
+                'message'=> $validatorAuthor->errors()
+            ],422);
+        }
+
+        // 3.Upload
+
+        $photo = $request -> file('photo');
+        $photo -> store('authors', 'public');
+
+        //4. insert
+       $author =  Author::create([
+            'name'=> $request ->name,
+            'photo'=> $photo ->hashName(),
+            'bio'=> $request ->bio
+        ]);
+
+        //5. response
+        return response()-> json([
+            'succes'=> true,
+            'message'=> 'resource added successfully',
+            'data'=> $author
+        ]);
+
     }
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
    protected $table = 'genres';
+
+   protected $fillable =[
+      'name','description'
+
+
+   ];
     
 }
